63f74941ca2746452a28f9cadefda55ef098610d
sheikhhasib <hasib2130@gmail.com>
2021-05-30 23:03:28 +0600

    almost facebook home page responsive done

---
+++ index.js
@@ -0,0 +1,28 @@
+function seeMoreFunc() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("seeBtn");
+    
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "<span class='see-more-icon' style='padding: 8px 10px;'><i class='fas fa-chevron-down '></i></span> See more"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "<span class='see-more-icon' style='padding: 8px 10px;'><i class='fas fa-chevron-up'></i></span>  See less"; 
+      moreText.style.display = "inline";
+    }
+  }
+
+
+  function mgsFunc() {
+    var messenger = document.getElementById("messenger");
+    var mgsButton = document.getElementById("mgsButton");
+    
+    if (messenger.style.display === "none") {
+      messenger.style.display = "block";
+    }else{
+      messenger.style.display = "none"
+    }
+    
+  }
