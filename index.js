52a874f2528f7dbeced4bd9ce47bec50e0f1374b
sheikhhasib <hasib2130@gmail.com>
2021-06-01 22:16:49 +0600

    story section create and all design modify

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function seeMoreFunc() {
     var dots = document.getElementById("dots");
     var moreText = document.getElementById("more");
     var btnText = document.getElementById("seeBtn");
-    
+
     if (dots.style.display === "none") {
       dots.style.display = "inline";
       btnText.innerHTML = "<span class='see-more-icon' style='padding: 8px 10px;'><i class='fas fa-chevron-down '></i></span> See more"; 
@@ -30,14 +30,27 @@ function seeMoreFunc() {
 
   function facebookInfoFunc() {
     var facebookInfo = document.getElementById("facebookInfo");
-    var infoButton = document.getElementById("infoButton");
+    var headerRight = document.getElementById("headerRight");
+    var headerMiddle = document.getElementById("headerMiddle");
+    var Story = document.getElementById("Story");
     
+    //this area for navbar 
+    if (headerRight.style.display === "none") {
+      headerRight.style.display = "block";
+      headerMiddle.style.display = "block";
+    }else{
+      headerRight.style.display = "none";
+      headerMiddle.style.display = "none";
+    }
+
+    //this area for left side bar 
     if (facebookInfo.style.display === "none") {
       facebookInfo.style.display = "block";
       facebookInfo.style.position = "relative";
       facebookInfo.style.marginTop = "0";
+      Story.style.margin = "100px 0 10px 0";
     }else{
-      facebookInfo.style.display = "none"
+      facebookInfo.style.display = "none";
+      Story.style.margin = "10px 0";
     }
-    
   }
